2b8e4feb5f460b71cae66c64b8266f188094d957
Luiz Siqueira <siqueiradw@gmail.com>
2022-08-11 07:08:09 -0300

    Project created

---
+++ src/main/java/com/siqueira/workshop/WorkshopSpringboot2JpaApplication.java
@@ -0,0 +1,13 @@
+package com.siqueira.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot2JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siqueira/workshop/WorkshopSpringboot2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siqueira.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot2JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
