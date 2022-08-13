b49598214b2ca38dcc2910b9d59d5bb8a93108b2
Luiz Siqueira <siqueiradw@gmail.com>
2022-08-13 07:41:56 -0300

    Product

---
+++ src/main/java/com/siqueira/workshop/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.siqueira.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.siqueira.workshop.entities.Product;
+
+public interface ProductRepository  extends JpaRepository<Product, Long>{
+}
