eef4af0b8edc50088cba9acc6f7f55b71622eed7
Luiz Siqueira <siqueiradw@gmail.com>
2022-08-15 11:28:19 -0300

    Exception handling - update

--- src/main/java/com/siqueira/workshop/services/UserService.java
+++ src/main/java/com/siqueira/workshop/services/UserService.java
@@ -3,6 +3,8 @@ package com.siqueira.workshop.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,10 +44,14 @@ public class UserService {
 		}
 	}
 
-	public User update(User id, User obj) {
-		User entity = id;
-		updateData(entity, obj);
-		return repository.save(entity);
+	public User update(Long id, User obj) {
+		try {
+			User entity = findById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
