4a829dd15ecb069d8eed96fc897f646ca40cba1f
Luiz Siqueira <siqueiradw@gmail.com>
2022-08-15 06:27:50 -0300

    Exception handling - delete

--- src/main/java/com/siqueira/workshop/services/UserService.java
+++ src/main/java/com/siqueira/workshop/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.siqueira.workshop.entities.User;
 import com.siqueira.workshop.repositories.UserRepository;
+import com.siqueira.workshop.services.exceptions.DatabaseException;
 import com.siqueira.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,11 +33,16 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(User id, User obj) {
-//		User entity = repository.getOne(id);
 		User entity = id;
 		updateData(entity, obj);
 		return repository.save(entity);
